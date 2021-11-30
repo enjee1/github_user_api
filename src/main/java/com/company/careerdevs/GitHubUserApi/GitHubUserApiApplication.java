8dd8379505b6fb62afd11315de8520069ff2e3e8
unknown <22105805+enjee1@users.noreply.github.com>
2021-11-30 18:09:41 -0500

    Created User

--- src/main/java/com/company/careerdevs/GitHubUserApi/GitHubUserApiApplication.java
+++ src/main/java/com/company/careerdevs/GitHubUserApi/GitHubUserApiApplication.java
@@ -1,13 +1,22 @@
 package com.company.careerdevs.GitHubUserApi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GitHubUserApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubUserApiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
