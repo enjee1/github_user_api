354e08690bf04db92a65ed8cdd388ceb489b0f8c
unknown <22105805+enjee1@users.noreply.github.com>
2021-11-30 17:52:12 -0500

    Initial commit

---
+++ src/main/java/com/company/careerdevs/GitHubUserApi/GitHubUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.careerdevs.GitHubUserApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/careerdevs/GitHubUserApi/GitHubUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.careerdevs.GitHubUserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
